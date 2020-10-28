84f53b9d235a6c565fdace7857e8e3f739335004
Lê Huy Vũ <35593308+huyvuqd@users.noreply.github.com>
2020-10-28 23:11:29 +0700

    Add category

---
+++ DemoCRUD/Controllers/CategoryController.cs
@@ -0,0 +1,73 @@
+﻿using DemoCRUD.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DemoCRUD.Controllers
+{
+    public class CategoryController : Controller
+    {
+       
+        // GET: Category
+        public ActionResult Index()
+        {
+            var db = new DemoCrudDbContext();
+            List<Category> categories = new List<Category>();
+            categories = db.Categories.ToList();
+            return View(categories);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        public ActionResult CreateData(Category category)
+        {
+            var db = new DemoCrudDbContext();
+            try
+            {
+                db.Categories.Add(category);
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var db = new DemoCrudDbContext();
+
+            try
+            {
+                var category = db.Categories.FirstOrDefault(x => x.Id == id);
+                if(category != null)
+                {
+                    db.Categories.Remove(category);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                     return RedirectToAction("E404", "Home");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public ActionResult Update()
+    }
+
+    
+}
